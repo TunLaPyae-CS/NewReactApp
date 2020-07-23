2eb317c72c4300d7f76bd1eeca7c2b0e7d0e67c2
TunLaPyae-CS <lapyae.iecm@gmail.com>
2020-07-23 13:53:30 +0630

    Hello

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello This is my first app</h1>
+      <p>this is pretty fucking ducking epic</p>
+    </div>
+  );
+}
+
+export default App;
